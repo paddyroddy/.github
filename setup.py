72691e7776993e6962a37eae91596dca11571abb
Paddy Roddy <patrickjamesroddy@gmail.com>
2023-10-30 18:29:10 +0000

    Include `setup.py`

--- setup.py
+++ setup.py
@@ -2,18 +2,6 @@ import setuptools
 
 
 setuptools.setup(
-    entry_points={
-        "console_scripts": [
-            "latex-hooks = precommit.latex.latex_hooks:main",
-            "python-hooks = precommit.python.python_hooks:main",
-        ]
-    },
     name="pre_commit_placeholder_package",
-    packages=setuptools.find_packages(
-        exclude=[
-            "actions",
-            "renovate",
-        ]
-    ),
     version="0.0.0",
 )
