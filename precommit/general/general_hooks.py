c059d5528cc15abb7aff845028e6f9feb9d76984
Patrick Roddy <patrickjamesroddy@gmail.com>
2023-11-05 16:18:05 +0000

    Add a general set of hooks (#93)

---
+++ precommit/general/general_hooks.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+"""General pre-commit hooks."""
+import pathlib
+import sys
+
+HERE = pathlib.Path(__file__).resolve()
+
+sys.path.append(str(HERE.parents[1]))
+
+from _run_hooks import run_hooks  # noqa: E402
+
+
+def main() -> int:
+    """
+    Run the general pre-commit hooks.
+
+    Returns
+        The return code of the process
+    """
+    return run_hooks(pathlib.Path("general/general-hooks.yml"))
+
+
+if __name__ == "__main__":
+    sys.exit(main())
