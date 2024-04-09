9a93dff871ef047957ddf1d1605837d06d22f30c
Patrick J. Roddy <patrickjamesroddy@gmail.com>
2024-04-09 13:09:00 +0100

    Change to YAML (#202)

--- precommit/general/general_hooks.py
+++ precommit/general/general_hooks.py
@@ -18,7 +18,7 @@ def main() -> int:
     Returns
         The return code of the process
     """
-    return run_hooks(pathlib.Path("general/general-hooks.yml"))
+    return run_hooks(pathlib.Path("general/general-hooks.yaml"))
 
 
 if __name__ == "__main__":
