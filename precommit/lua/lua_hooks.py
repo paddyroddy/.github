9b2cfc8f2617d0e1b2ad152bc92a00c866459342
Patrick J. Roddy <patrickjamesroddy@gmail.com>
2024-09-06 10:04:10 +0100

    Add `lua` `pre-commit` hooks

---
+++ precommit/lua/lua_hooks.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+"""Lua pre-commit hooks."""
+
+import pathlib
+import sys
+
+HERE = pathlib.Path(__file__).resolve()
+
+sys.path.append(str(HERE.parents[1]))
+
+from _run_hooks import run_hooks  # noqa: E402
+
+
+def main() -> int:
+    """
+    Run the Lua pre-commit hooks.
+
+    Returns
+        The return code of the process
+    """
+    return run_hooks(pathlib.Path("lua/lua-hooks.yaml"))
+
+
+if __name__ == "__main__":
+    sys.exit(main())
